1709bcfd66a7c73e67d500ee7dc34ab45bb2f478
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2025-01-03 19:07:31 -0300

    feat: create main page

---
+++ localize/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-4xl font-bold">Bem vindo a Localize</h1>
+        <p className="mt-4">Procure eventos para você!</p>
+      </main>
+    </div>
+  );
+}
