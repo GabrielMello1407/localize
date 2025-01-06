6e57ee8a90ddd304a9fed9a4220038de22caa7a5
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2025-01-06 18:01:36 -0300

    feat: added new domain request

--- localize/next.config.js
+++ localize/next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
