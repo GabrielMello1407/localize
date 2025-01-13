233ba5360cfa72d6ac6e2183b33df3e351ae0445
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2025-01-13 18:32:29 -0300

    style: changed position

--- localize/app/layout.tsx
+++ localize/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Toaster
-          position="bottom-right"
+          position="top-center"
           reverseOrder={false}
           toastOptions={{
             success: {
