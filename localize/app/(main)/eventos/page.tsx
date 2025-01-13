f758ac6bab1465ae72f2f51d2ef5a5f8dd129af1
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2025-01-13 18:35:09 -0300

    feat: added filter

--- localize/app/(main)/eventos/page.tsx
+++ localize/app/(main)/eventos/page.tsx
@@ -1,9 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import EventGroup from '@/components/event-group';
+import axios from 'axios';
+import EventFilter from '@/components/event-filter';
 
 const Events = () => {
+  const [events, setEvents] = useState([]);
+  const searchParams = useSearchParams();
+  const filter = searchParams.get('txt_busca') || '';
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get(`/api/events?txt_busca=${filter}`);
+        setEvents(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar eventos:', error);
+      }
+    };
+
+    fetchEvents();
+  }, [filter]);
+
   return (
-    <div className="container mx-auto px-4 py-8">
-      <EventGroup />
+    <div className="container mx-auto px-4 py-8 cursor-pointer">
+      <EventGroup events={events} />
     </div>
   );
 };
