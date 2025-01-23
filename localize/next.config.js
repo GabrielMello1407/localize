d4a93f40160977b4812a92cb94cff2cb4ccb0abd
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2025-01-23 16:53:49 -0300

    feat: added route

--- localize/next.config.js
+++ localize/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com', 'api.qrserver.com'],
     remotePatterns: [
       {
         protocol: 'https',
